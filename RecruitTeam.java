9f7a0daa9252d713d82766905c64335dd53b5c39
keerthana1625 <65592959+keerthana1625@users.noreply.github.com>
2023-05-10 22:13:22 +0530

    Update RecruitTeam.java

--- RecruitTeam.java
+++ RecruitTeam.java
@@ -4,20 +4,17 @@ import java.util.List;
 public class RecruitTeam {
     private List<Candidate> candidates = new ArrayList<>();
 
-    // Method to add a candidate to the list
+  
     public void addCandidate(Candidate candidate) {
         candidates.add(candidate);
     }
 
-    // Method to calculate the final marks of each candidate and add them to the HR
-    // team's list
+ 
     public List<Candidate> calculateFinalMarks() {
         List<Candidate> eligibleCandidates = new ArrayList<>();
         List<Candidate> noteligibleCandidates = new ArrayList<>();
         for (Candidate candidate : candidates) {
-            // double finalMarks = (candidate.getHscAggregate() * 0.5) +
-            // (candidate.getInterviewScore() * 0.35)
-            // + (candidate.getNumProjects() * 0.15);
+           
             if (candidate.getPcmPcbAverage() >= 50 && candidate.getUgCgpa() >= 8
                     && candidate.getPgCgpa() >= 8
                     && candidate.isFullTime() && candidate.isIndianCitizen()
